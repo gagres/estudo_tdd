31af17cd7384512d9706149d338638cd911c9073
Gabriel Giro Resende <gabriel.giro.resende@outlook.com>
2017-12-31 14:34:51 -0200

    Added config files

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const a = 2;
+const b = 4;
+
+const c = 'aasjgdahsgdahsgdahgsdjhagshdgasjhdgajshdgahsgdajshgdjahsgdhasgdjashgdjashgdhasd';
+
+function soma() {
+  return a + b + c;
+}
+
+soma();
+
+if (a === 2) {
+  console.log(a);
+}
